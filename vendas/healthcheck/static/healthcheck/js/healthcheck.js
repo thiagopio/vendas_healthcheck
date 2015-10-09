36df06dcf99f846f8f6c60df1a4396f2a9b22677
Thiago Pio <alemdolimite@gmail.com>
2015-10-09 01:15:40 -0300

    Refactor javascript

--- vendas/healthcheck/static/healthcheck/js/healthcheck.js
+++ vendas/healthcheck/static/healthcheck/js/healthcheck.js
@@ -2,15 +2,17 @@
  * Created by thiago.pio on 9/27/15.
  */
  var healthcheck = function(){
-    $html_ul = $('<ul></ul>');
-    $html_site = $('<li>{url}</li>');
+    public = {},
+    public.ok_class = 'project-green',
+    public.problem_class = 'project-red',
+    public.warn_class = 'project-yellow',
+    $html_ul = $('<ul></ul>'),
+    $html_site = $('<li>{url}</li>'),
+    local_id = 'body',
     refresh_status_time = 5000;
-    ok_class = 'project-green';
-    problem_class = 'project-red';
-    warn_class = 'project-yellow';
 
-    verify_status_class = function(status){
-        return status == 200 ? ok_class : problem_class;
+    public.verify_status_class = function(status){
+        return status == 200 ? public.ok_class : public.problem_class;
     };
 
     verify_dependents = function(data, status_class){
@@ -18,8 +20,8 @@
         $current.attr('data-problem-pk', null);
         data.dependents_ids.every(function(value){
             $dependent = $('li[data-pk=' + value + ']');
-            if (status_class == ok_class && !$dependent.is('.' + ok_class)) {
-                status_class = warn_class;
+            if (status_class == public.ok_class && !$dependent.is('.' + public.ok_class)) {
+                status_class = public.warn_class;
                 $current.attr('data-problem-pk', value);
                 return;
             }
@@ -28,18 +30,18 @@
     };
 
     update_status_class = function($html, data){
-        var status_class = verify_status_class(data.status);
-        $html.removeClass(ok_class);
-        $html.removeClass(problem_class);
-        $html.removeClass(warn_class);
+        var status_class = public.verify_status_class(data.status);
+        $html.removeClass(public.ok_class);
+        $html.removeClass(public.problem_class);
+        $html.removeClass(public.warn_class);
         if (data.status != 404) {
             status_class = verify_dependents(data, status_class);
             $html.addClass(status_class);
         }
-        update_project_name($html, data);
+        public.update_project_name($html, data);
     };
 
-    update_project_name = function($html, data){
+    public.update_project_name = function($html, data){
         var $problem_project, problem_project_id;
         if (data.status == 200) {
             $html.text(data.name);
@@ -60,11 +62,11 @@
             $.getJSON( '/healthcheck/project/' + project_id + '/json/', function( response ) {
                 update_status_class($project, response)
             });
-        });
+        });verify_dependents
         setTimeout(function(){ refresh_status() }, refresh_status_time);
     };
 
-    create_box_for = function(data){
+    public.create_box_for = function(data){
         $html_clone = $html_site.clone();
         $html_clone.addClass('project');
         $html_clone.attr('data-pk', data.id);
@@ -73,27 +75,20 @@
         return $html_clone;
     };
 
-    init = function(){
+    public.init = function(){
         $.getJSON( "/healthcheck/projects/all/json/", function( environment ) {
             $.each(environment, function(env, projects){
                 var title = '<h2>' + env + '</h2>';
                 var $html = $html_ul.clone().attr('id', env);
                 $.each(projects, function(key, project) {
-                    $project_item = create_box_for(project);
+                    $project_item = public.create_box_for(project);
                     $html.append($project_item);
                 });
-                $('body').append(title, $html);
+                $(local_id).append(title, $html);
             });
             setTimeout(function(){ refresh_status() }, refresh_status_time);
         });
     };
 
-    return {
-        'attr_ok_class': ok_class,
-        'attr_problem_class': problem_class,
-        'fn_verify_status_class': verify_status_class,
-        'fn_create_box_for': create_box_for,
-        'fn_update_project_name': update_project_name,
-        'init': init
-    }
+    return public;
  }();
